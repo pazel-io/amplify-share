813b5341cf652a75d524d775488e92a444d9166d
Parham <parham.fazel@spatialvision.com.au>
2022-01-12 14:39:12 +1100

    move file to project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyshare": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
