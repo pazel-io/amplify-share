ff7896efa14a5a7809014d713317094d93695484
Parham <parham.fazel@spatialvision.com.au>
2022-01-12 14:05:50 +1100

    initial commit

---
+++ apps/amplify-share/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amplify-share-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'amplify-share';
+}
