36dbce2f2234ddae40c34f5b155e29dc604f5fe5
Parham <parham.fazel@spatialvision.com.au>
2022-01-13 12:06:05 +1100

    add amplify to prod env

--- apps/amplify-share/src/environments/environment.prod.ts
+++ apps/amplify-share/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+// @ts-ignore
+import amplify from '../aws-exports';
+// @ts-ignore
+import packageInfo from '../../../../package.json';
+
 export const environment = {
-  production: true,
+  production: false,
+  amplify,
+  version: packageInfo.version,
+  buildNumber: 'UNKNOWN',
 };
